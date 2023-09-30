f58ac1e8dc8dc2d86f6024f1a97be14a5c89ff09
Nikita Gashkov <nikita@gashkov.com>
2023-09-30 15:02:36 +0200

    Improve list.py output by merging repeated items

--- list.py
+++ list.py
@@ -1,9 +1,16 @@
+from collections import defaultdict
 import os
 import pathlib
 import re
 import argparse
+from typing import NamedTuple
 
 pattern = re.compile(r"```ingredients(.+?)```", re.DOTALL)
+
+
+class Ingredient(NamedTuple):
+    name: str
+    quantity: str
 
 
 def parse_ingredients(text: str) -> list[str]:
@@ -12,7 +19,8 @@ def parse_ingredients(text: str) -> list[str]:
 
     for match in pattern.findall(text):
         for line in match.strip().splitlines():
-            ingredients.append(line.strip())
+            name, quantity = line.strip().split(":", maxsplit=1)
+            ingredients.append(Ingredient(name=name, quantity=quantity))
 
     return ingredients
 
@@ -28,7 +36,16 @@ def main() -> None:
     for path in args.paths:
         ingredients.extend(parse_ingredients(path.read_text()))
 
-    print(os.linesep.join(sorted(ingredients)))
+    quantities_by_name = defaultdict(list)
+
+    for ingredient in ingredients:
+        if quantity := ingredient.quantity.strip():
+            quantities_by_name[ingredient.name].append(quantity)
+
+    max_len = max([len(k) for k in quantities_by_name.keys()])
+
+    for name, quantities in sorted(quantities_by_name.items()):
+        print(name.ljust(max_len+2), " + ".join(quantities))
 
 
 if __name__ == "__main__":
